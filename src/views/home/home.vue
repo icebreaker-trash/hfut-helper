7ca64641c342d5b6b2a8e402e5779dbdadde8a02
snowingfox <1503401882@qq.com>
2022-04-14 21:11:43 +0800

    :sparkles: feat: add home page

---
+++ src/views/home/home.vue
@@ -0,0 +1,21 @@
+<script lang='ts' setup>
+import { onLoad } from '@dcloudio/uni-app'
+import { useAsync } from '@/hooks/use-async'
+import { getCourseListRequest } from '@/service/api/eam'
+
+onLoad(async() => {
+  console.log('launch')
+  const { state, error } = await useAsync(getCourseListRequest)
+  if (state.value) {
+    console.log(state.value)
+  }
+})
+</script>
+
+<template>
+  <view>hello home</view>
+</template>
+
+ <style lang='scss' scoped>
+
+</style>
