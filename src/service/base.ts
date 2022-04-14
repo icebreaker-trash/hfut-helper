fd9f4ae1a0726e8a912fde3a0b8bf10100b8fc9f
snowingfox <1503401882@qq.com>
2022-04-14 21:16:08 +0800

    :sparkles: feat: handle cookie

--- src/service/base.ts
+++ src/service/base.ts
@@ -2,12 +2,36 @@ import type { AjaxInstance, AjaxRequestConfig } from 'uni-ajax'
 import ajax from 'uni-ajax'
 import { requestConfig } from '@/shared/config/request'
 
+const headers = {
+  cookie: '',
+}
+
 export function createInstance(config: AjaxRequestConfig) {
   return ajax.create(config)
 }
 
 const baseInstance = createInstance({
   baseURL: requestConfig.baseURL,
+})
+
+baseInstance.interceptors.request.use((config: AjaxRequestConfig) => {
+  config.header = {
+    ...config.header,
+    cookie: headers.cookie,
+  }
+  return config
+})
+
+baseInstance.interceptors.response.use((res) => {
+  const cookies = res.cookies
+  cookies.forEach((cookie) => {
+    if (cookies.includes('wengine_vpn_ticketwebvpn_hfut_edu_cn')) {
+      headers.cookie = cookie
+    } else {
+      headers.cookie = cookie
+    }
+  })
+  return res
 })
 
 export function request(config: AjaxRequestConfig, instance: AjaxInstance<any> = baseInstance) {
