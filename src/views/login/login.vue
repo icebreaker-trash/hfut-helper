b5af8616dc497bd6d844b7ebf61b1859fcaf0fd4
snowingfox <1503401882@qq.com>
2022-04-14 21:11:20 +0800

    :zap: chore: use hook

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,92 +1,43 @@
 <script lang='ts' setup>
-import { reactive } from 'vue'
-import * as _ from 'lodash'
-import Notify from '@vant/weapp/dist/notify/notify'
 import Toast from '@vant/weapp/dist/toast/toast'
+import type { AjaxResponse } from 'uni-ajax'
 import img from '@/views/login/img.png'
 import BaseButton from '@/components/base/BaseButton.vue'
 import Tip from '@/components/base/Tip.vue'
-import { requestConfig } from '@/shared/config/request'
+import { loginRequest } from '@/service/api/auth'
+import { useAsync } from '@/hooks/use-async'
+import { useFormValidate } from '@/views/login/use-formValidate'
 
-const form = reactive({
-  username: '',
-  password: '',
-})
+const { validate, form } = useFormValidate()
 
-// 考虑到小程序功能没有多少表单验证，所以没必要装插件，直接手撸吧
-function validate() {
-  const username = form.username
-
-  const validateUsername = () => {
-    if (_.isNaN(_.toNumber(username))) {
-      return
-    }
-    if (username.length !== 10) {
-      return
-    }
-    if (!username.startsWith('20')) {
-      return
-    }
-
-    return true
-  }
-
-  const validatePwd = () => {
-    const pwd = form.password
-    if (pwd.length < 13) {
-      return
-    }
-    return true
-  }
-
-  const isValidate = {
-    username: validateUsername(),
-    pwd: validatePwd(),
-  }
-  if (!isValidate.username) {
-    Notify({ type: 'danger', message: '学号格式错误' })
-    return
-  }
-
-  if (!isValidate.pwd) {
-    Notify({ type: 'danger', message: '密码格式错误，信息门户的密码至少是13位字符' })
-    return
-  }
-
-  return true
-}
-
-function onLogin() {
+async function onLogin() {
   if (validate()) {
     Toast.loading({
       duration: 0,
       message: '第一次登录会花些时间，请耐心等待...\n(๑•̀ㅂ•́)و✧',
       forbidClick: true,
     })
-    uni.request({
-      url: `${requestConfig.baseURL}login?username=${form.username}&password=${form.password}`,
-      method: 'GET',
-      success(res) {
-        if (res.statusCode !== 200) {
-          Toast.fail({
-            forbidClick: true,
-            duration: 3000,
-            message: `Σ(｀д′*ノ)ノ\n${(res.data as any).msg}`,
-          })
-        } else {
-          Toast.success({
-            duration: 3000,
-            message: '<(￣︶￣)>\n登录成功',
-          })
-        }
-      },
-      fail(res) {
-        Toast.fail({
-          duration: 3000,
-          message: '.....((/- -)/\n登录失败了...',
-        })
-      },
-    })
+
+    const { state, error } = await useAsync(loginRequest({
+      username: form.username,
+      password: form.password,
+    }) as any)
+
+    if (error.value) {
+      Toast.fail({
+        message: (error.value as AjaxResponse).data.msg,
+      })
+      return
+    }
+
+    if (state.value) {
+      Toast.success({
+        message: '登录成功',
+      })
+      uni.redirectTo({
+        url: '/views/home/home',
+      })
+    }
   }
 }
 
