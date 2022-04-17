f4e50e1919bdb044971533fb0130e398998d2109
snowingfox <1503401882@qq.com>
2022-04-17 13:27:38 +0800

    feat: add loading

---
+++ src/components/base/Loading.vue
@@ -0,0 +1,24 @@
+<script lang='ts' setup>
+interface Props {
+  content: string
+  loading: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  content: '加载中',
+  loading: true,
+})
+
+</script>
+
+<template>
+  <view v-if="props.loading">
+    <van-loading vertical color="#1989fa">
+      {{ props.content }}
+    </van-loading>
+  </view>
+</template>
+
+<style lang='scss' scoped>
+
+</style>
