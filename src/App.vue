b57d0533775b265ae934477a2de62c19a8af7753
snowingfox <1503401882@qq.com>
2022-04-20 16:33:20 +0800

    chore: delete useless code

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <script setup lang="ts">
 import { onHide, onLaunch, onShow } from '@dcloudio/uni-app'
-import { courseList } from '@/_mock_/auth'
-import { useCourseListStore } from '@/store/courseList.store'
-
-const store = useCourseListStore()
 onLaunch(() => {
 
 })
@@ -13,7 +9,6 @@ onShow(() => {
 onHide(() => {
   console.log('App Hide')
 })
-store.setCourse(courseList.result as any)
 </script>
 
 <template>
