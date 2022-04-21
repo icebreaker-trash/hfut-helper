557ed4e0c8141b6062b228800a48ad42b8c3dfde
snowingfox <1503401882@qq.com>
2022-04-21 11:11:10 +0800

    feat: add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,41 @@
 import { join } from 'path'
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
+
+import { postcssWeappTailwindcssRename, ViteWeappTailwindcssPlugin as vwt } from 'weapp-tailwindcss-webpack-plugin'
+
+const postcssPlugins = [require('autoprefixer')(), require('tailwindcss')()]
+
+postcssPlugins.push(
+  require('postcss-rem-to-responsive-pixel')({
+    rootValue: 32,
+    propList: ['*'],
+    transformUnit: 'rpx',
+  }),
+)
+postcssPlugins.push(postcssWeappTailwindcssRename({
+  cssPreflight: {
+    'box-sizing': false,
+  },
+}))
+
 function resolve(dir: string): string {
   return join(__dirname, dir)
 }
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [vwt(), uni()],
   resolve: {
     alias: {
       '@': resolve('src/'),
     },
   },
+
   css: {
+    postcss: {
+      plugins: postcssPlugins,
+    },
     preprocessorOptions: {
       scss: {
         additionalData: `
